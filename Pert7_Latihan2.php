f98ab41c201d65de53132c18f622508adc8a766a
mfahmifauzi13 <80532013+mfahmifauzi13@users.noreply.github.com>
2021-05-01 10:36:12 +0700

    Create Pert7_Latihan2.php

---
+++ Pert7_Latihan2.php
@@ -0,0 +1,21 @@
+<htmnl>
+<head>
+    <title> Penggunaan List </title>
+</head>
+
+<body>
+    <?php
+        $program = array('Bobo', 'Doraemon', 'Spiderman');
+        list($Majalah, $Komik, $Film) = $program;
+        echo "Jenis Buku & Hiburan :";
+        echo "<br />";
+        echo "Cerpen : $Majalah";
+        echo "<br />";
+        echo "Cerita Bergambar : $Komik";
+        echo "<br />";
+        echo "Bioskop : $Film";
+    ?>
+</body>
+
+</htmnl>
+© 2021 GitHub, Inc.
